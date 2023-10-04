7b10552d2bcee96c439449683201573218ba4813
Doug Woodrow <doug@dougwoodrow.com>
2023-10-03 23:51:36 -0400

    Datetime strings for current request now returned

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate serde_json;
 use rocket::serde::json::Json;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
+use chrono::{Utc};
 
 #[derive(Serialize, Deserialize)]
 struct Person {
@@ -23,7 +24,8 @@ fn create_person(person: Json<Person>) -> Json<serde_json::Value> {
     let response = json!({
         "message": format!("Hello, {} year old named {}!", person.age, person.name),
         "is_adult": person.age >= 18,
-        "timestamp": "2023-10-03T12:34:56Z"
+         // get current timestamp here
+        "timestamp": Utc::now().to_rfc3339(),
     });
 
     Json(response)
